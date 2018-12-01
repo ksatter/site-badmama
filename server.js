558e020b753bac9753c7bcbb9849a51804a0ea78
Katheryn Satterlee <me@ksatter.com>
2018-11-30 23:16:23 -0600

    adding express server for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 
-app.get('*', (req, res) => res.send("./index.html"))
+app.get('*', (req, res) => res.sendfile(path.join(__dirname +'/public/index.html')));
 
 app.listen(PORT, () =>
   console.log(`App listening on port ${PORT}`)
